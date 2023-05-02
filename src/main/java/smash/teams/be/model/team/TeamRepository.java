b65ea1a0190137f440267370198ade7a38b2f3b7
dltmdalssssss <dltmdalssssss0520@gmail.com>
2023-05-02 17:27:19 +0900

    Feat: 팀 추가, 팀 삭제 구현 완료

--- src/main/java/smash/teams/be/model/team/TeamRepository.java
+++ src/main/java/smash/teams/be/model/team/TeamRepository.java
@@ -1,6 +1,12 @@
 package smash.teams.be.model.team;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
+    @Query("select t from Team t where t.team = :team")
+    Optional<Team> findByTeam(@Param("team") String team);
 }
---
+++ src/main/java/smash/teams/be/service/TeamService.java
@@ -0,0 +1,52 @@
+package smash.teams.be.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import smash.teams.be.core.exception.Exception400;
+import smash.teams.be.core.exception.Exception500;
+import smash.teams.be.dto.team.TeamRequest;
+import smash.teams.be.model.team.Team;
+import smash.teams.be.model.team.TeamRepository;
+import smash.teams.be.model.user.UserRepository;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class TeamService {
+    private final TeamRepository teamRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public Team add(TeamRequest.AddDTO addDTO) {
+        Optional<Team> teamOP = teamRepository.findByTeam(addDTO.getTeam());
+        if (teamOP.isPresent()) {
+            throw new Exception400(addDTO.getTeam(), "이미 존재하는 팀입니다.");
+        }
+        try {
+            Team teamPS = teamRepository.save(addDTO.toEntity());
+            return teamPS;
+        } catch (Exception e) {
+            throw new Exception500("팀 추가 실패 : " + e.getMessage());
+        }
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Optional<Team> teamOP = teamRepository.findById(id);
+        if (!teamOP.isPresent()) {
+            throw new Exception400(id + "", "존재하지 않는 팀입니다.");
+        }
+        // 팀에 소속된 인원이 1명이라도 있을 경우 팀 삭제 불가
+        int count = userRepository.calculateCountById(teamOP.get().getId());
+        if (count > 0) {
+            throw new Exception400(id + "", "팀에 소속된 인원이 1명 이상입니다.");
+        }
+        try {
+            teamRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new Exception500("팀 삭제 실패 : " + e.getMessage());
+        }
+    }
+}
