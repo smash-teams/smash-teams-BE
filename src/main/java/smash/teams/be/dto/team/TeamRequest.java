3b4526726d8ea90fa2c30410bf9544e7de9de2ad
dltmdalssssss <dltmdalssssss0520@gmail.com>
2023-05-03 17:25:12 +0900

    Fix: TeamRequest Validation 추가, DummyEntity 수정

--- src/main/java/smash/teams/be/dto/team/TeamRequest.java
+++ src/main/java/smash/teams/be/dto/team/TeamRequest.java
@@ -4,10 +4,15 @@ import lombok.Getter;
 import lombok.Setter;
 import smash.teams.be.model.team.Team;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 public class TeamRequest {
     @Getter
     @Setter
     public static class AddInDTO {
+        @Pattern(regexp = "^[가-힣]{19}팀$") // 오직 한글, 마지막 글자가 "팀"으로 끝나면서 글자 수 제한이 20
+        @NotEmpty
         private String teamName;
 
         public Team toEntity() {
