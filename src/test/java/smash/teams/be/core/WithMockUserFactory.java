29c4300b8fd24268985f7f57428a4e52e986709e
BE_YangJinho <real.great.code@gmail.com>
2023-05-07 16:38:32 +0900

    Test: ScheduleController getScheduleListForManage_test 완료

--- src/test/java/smash/teams/be/core/WithMockUserFactory.java
+++ src/test/java/smash/teams/be/core/WithMockUserFactory.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithSecurityContextFactory;
 import smash.teams.be.core.auth.session.MyUserDetails;
+import smash.teams.be.model.team.Team;
 import smash.teams.be.model.user.Role;
 import smash.teams.be.model.user.Status;
 import smash.teams.be.model.user.User;
@@ -18,15 +19,18 @@ public class WithMockUserFactory implements WithSecurityContextFactory<WithMockU
         SecurityContext context = SecurityContextHolder.createEmptyContext();
         User user = User.builder()
                 .id(mockUser.id())
-                .name("kimuser")
+                .name(mockUser.username().replace("@gmail",""))
                 .password("1234")
                 .email(mockUser.username())
                 .phoneNumber("010-1234-5678")
                 .remain(20)
-                .role(Role.USER.getRole())
+                .role(mockUser.role())
                 .status(Status.ACTIVE.getStatus())
                 .startWork(LocalDateTime.now())
                 .createdAt(LocalDateTime.now())
+                .team(Team.builder()
+                        .id(mockUser.teamId())
+                        .teamName(mockUser.teamName()).build())
                 .build();
         MyUserDetails myUserDetails = new MyUserDetails(user);
         Authentication auth = new UsernamePasswordAuthenticationToken(myUserDetails, myUserDetails.getPassword(), myUserDetails.getAuthorities());
