3a94db697b59989f060a26e02218f3e876306191
dltmdalssssss <dltmdalssssss0520@gmail.com>
2023-05-07 16:52:54 +0900

    Fix: 테스트 관련 오류 해결 및 @Transactional(readOnly = true) 추가

--- src/main/java/smash/teams/be/controller/ScheduleController.java
+++ src/main/java/smash/teams/be/controller/ScheduleController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import smash.teams.be.core.annotation.Log;
 import smash.teams.be.core.auth.session.MyUserDetails;
 import smash.teams.be.dto.ResponseDTO;
 import smash.teams.be.dto.schedule.ScheduleResponse;
@@ -16,6 +17,7 @@ public class ScheduleController {
 
     private final ScheduleService scheduleService;
 
+    @Log
     @GetMapping("/auth/user/schedule")
     public ResponseEntity<?> getScheduleList(@AuthenticationPrincipal MyUserDetails myUserDetails){
         Long userId = myUserDetails.getUser().getId();
--- src/main/java/smash/teams/be/service/ScheduleService.java
+++ src/main/java/smash/teams/be/service/ScheduleService.java
@@ -2,6 +2,8 @@ package smash.teams.be.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import smash.teams.be.core.annotation.Log;
 import smash.teams.be.core.exception.Exception404;
 import smash.teams.be.dto.schedule.ScheduleResponse;
 import smash.teams.be.model.schedule.Schedule;
@@ -16,13 +18,15 @@ public class ScheduleService {
 
     private final ScheduleRepository scheduleRepository;
 
-    public ScheduleResponse.ScheduleListDTO getScheduleList(Long userId){
+    @Log
+    @Transactional(readOnly = true)
+    public ScheduleResponse.ScheduleListDTO getScheduleList(Long userId) {
         List<Schedule> schedules = scheduleRepository.findSchedulesByUserId(userId);
-        if(schedules.isEmpty()){
+        if (schedules.isEmpty()) {
             throw new Exception404("스케쥴을 찾을 수 없습니다.");
         }
         List<ScheduleResponse.ScheduleOutDTO> scheduleOutDTOList = new ArrayList<>();
-        for(Schedule schedule : schedules){
+        for (Schedule schedule : schedules) {
             ScheduleResponse.UserOutDTOWithScheduleOutDTO userOutDTOWithScheduleOutDTO = new ScheduleResponse.UserOutDTOWithScheduleOutDTO(schedule.getUser());
             scheduleOutDTOList.add(new ScheduleResponse.ScheduleOutDTO(schedule, userOutDTOWithScheduleOutDTO));
         }
