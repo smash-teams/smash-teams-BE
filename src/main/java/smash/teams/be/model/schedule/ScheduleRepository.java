49f78063478abcc5ecdb9519c0286a81d11dfaaf
BE_YangJinho <real.great.code@gmail.com>
2023-05-07 15:30:03 +0900

    Feat: 관리자 스케쥴 조회 구현 완료

--- src/main/java/smash/teams/be/model/schedule/ScheduleRepository.java
+++ src/main/java/smash/teams/be/model/schedule/ScheduleRepository.java
@@ -7,6 +7,12 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+    @Query("SELECT s FROM Schedule s")
+    List<Schedule> findSchedules();
+
     @Query("SELECT s FROM Schedule s WHERE s.user.id = :userId")
     List<Schedule> findSchedulesByUserId(@Param("userId") Long userId);
+
+    @Query("SELECT s FROM Schedule s WHERE s.user.team.teamName = :teamName")
+    List<Schedule> findSchedulesByTeamName(@Param(teamName) String teamName);
 }
