961e32fbb2039b1b2832b5d02a69ca961f460611
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-05-08 17:50:26 +0900

    Test: UserController 테스트 글자 수정

--- src/test/java/smash/teams/be/service/UserServiceTest.java
+++ src/test/java/smash/teams/be/service/UserServiceTest.java
@@ -1,4 +1,44 @@
 package smash.teams.be.service;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+import smash.teams.be.model.user.UserRepository;
+
+@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
+
+    // 진짜 userService 객체를 만들고 Mock로 Load된 모든 객체를 userService에 주입
+    @InjectMocks
+    private UserService userService;
+
+    // 진짜 객체를 만들어서 Mockito 환경에 Load(userService에 주입)
+    @Mock
+    private UserRepository userRepository;
+
+    // 가짜 객체를 만들어서 Mockito 환경에 Load(userService에 주입)
+    @Mock
+    private AuthenticationManager authenticationManager;
+
+    // 진짜 객체를 만들어서 Mockito 환경에 Load
+    @Spy
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Test
+    public void findMyId_test() throws Exception {
+        // given
+
+
+        // when
+
+
+        // then
+    }
 }
