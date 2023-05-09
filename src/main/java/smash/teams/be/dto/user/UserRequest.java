ef33ac96393c6bedccdf832f756f8acfefc9f97b
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-05-09 22:23:43 +0900

    Feat: 기능update내개인 정보 수정 기능 완료

---
+++ src/main/java/smash/teams/be/dto/user/UserRequest.java
@@ -0,0 +1,44 @@
+package smash.teams.be.dto.user;
+
+import lombok.Getter;
+import lombok.Setter;
+import smash.teams.be.model.user.User;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class UserRequest {
+
+    @Getter
+    @Setter
+    public static class UpdateInDto {
+        @Size(min = 4, max = 20)
+        @NotEmpty
+        private String curPassword;
+        @Size(min = 4, max = 20)
+        @NotEmpty
+        private String newPassword;
+        @NotEmpty
+        private String phoneNumber;
+        @NotEmpty
+        private String startWork;
+        @NotEmpty
+        private String profileImage;
+
+        public User toEntity() {
+            LocalDate startDate = LocalDate.parse(startWork, DateTimeFormatter.ISO_LOCAL_DATE);
+            LocalDateTime startDateTime = LocalDateTime.of(startDate, LocalTime.MIDNIGHT);
+
+            return User.builder()
+                    .password(newPassword)
+                    .phoneNumber(phoneNumber)
+                    .startWork(startDateTime)
+                    .profileImage(profileImage)
+                    .build();
+        }
+    }
+}
--- src/main/java/smash/teams/be/service/UserService.java
+++ src/main/java/smash/teams/be/service/UserService.java
@@ -1,9 +1,13 @@
 package smash.teams.be.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import smash.teams.be.core.annotation.Log;
 import smash.teams.be.core.exception.Exception400;
+import smash.teams.be.core.exception.Exception500;
+import smash.teams.be.dto.user.UserRequest;
 import smash.teams.be.dto.user.UserResponse;
 import smash.teams.be.model.user.User;
 import smash.teams.be.model.user.UserRepository;
@@ -14,11 +18,41 @@ import smash.teams.be.model.user.UserRepository;
 public class UserService {
 
     private UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public UserResponse.findMyInfoOutDTO findMyId(Long id) {
         User userPS = userRepository.findById(id).orElseThrow(
                 () -> new Exception400("id", "해당 유저를 찾을 수 없습니다")
         );
         return new UserResponse.findMyInfoOutDTO(userPS);
+    }
+
+    @Log
+    @Transactional
+    public UserResponse.UpdateOutDTO update(Long userId, UserRequest.UpdateInDto updateInDto) {
+        User userPS = userRepository.findById(userId).orElseThrow(
+                () -> new Exception400("id", "해당 유저를 찾을 수 없습니다.")
+        );
+
+        if (!bCryptPasswordEncoder.matches(updateInDto.getCurPassword(), userPS.getPassword())) {
+            throw new Exception400("password", "비밀번호가 일치하지 않습니다.");
+        } // 비밀번호 일치
+
+        try {
+            User userEntity = updateInDto.toEntity();
+
+            String rawPassword = userEntity.getPassword();
+            String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+
+            userPS.updatePassword(encPassword);
+            userPS.updatePhoneNumber(userEntity.getPhoneNumber());
+            userPS.updateStartWork(userEntity.getStartWork());
+            userPS.updateProfileImage(userEntity.getProfileImage());
+
+            userRepository.save(userPS);
+            return new UserResponse.UpdateOutDTO(userPS);
+        } catch (Exception e) {
+            throw new Exception500("유저 변경 실패 : " + e.getMessage());
+        }
     }
 }
