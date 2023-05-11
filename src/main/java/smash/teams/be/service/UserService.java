bad2cb3578ef4602307097db85cc94ddc12fcddd
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-05-11 16:41:43 +0900

    Feat: uploadImage 기능 구현

--- src/main/java/smash/teams/be/service/UserService.java
+++ src/main/java/smash/teams/be/service/UserService.java
@@ -1,18 +1,21 @@
 package smash.teams.be.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 import smash.teams.be.core.annotation.Log;
 import smash.teams.be.core.auth.jwt.JwtProvider;
 import smash.teams.be.core.auth.session.MyUserDetails;
 import smash.teams.be.core.exception.Exception400;
 import smash.teams.be.core.exception.Exception401;
 import smash.teams.be.core.exception.Exception500;
+import smash.teams.be.core.util.FileUtil;
 import smash.teams.be.dto.user.UserRequest;
 import smash.teams.be.dto.user.UserResponse;
 import smash.teams.be.model.team.TeamRepository;
@@ -27,6 +30,9 @@ public class UserService {
     private final TeamRepository teamRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final AuthenticationManager authenticationManager;
+
+    @Value("${file.path}")
+    private String uploadFolder;
 
     @Log
     public UserResponse.LoginOutDTO login(UserRequest.LoginInDTO loginInDTO) {
@@ -82,6 +88,21 @@ public class UserService {
             return new UserResponse.UpdateOutDTO(userPS);
         } catch (Exception e) {
             throw new Exception500("유저 변경 실패 : " + e.getMessage());
+        }
+    }
+
+    @Log
+    @Transactional
+    public User uploadImage(MultipartFile profileImage, Long id) {
+        try {
+            String uuidImageName = FileUtil.write(uploadFolder, profileImage);
+
+            User userPS = userRepository.findById(id)
+                    .orElseThrow(() -> new Exception500("로그인 된 유저가 DB에 존재하지 않습니다."));
+            userPS.uploadImage(uuidImageName);
+            return userPS;
+        } catch (Exception e) {
+            throw new Exception500("프로필 사진 등록 실패 : " + e.getMessage());
         }
     }
 }
