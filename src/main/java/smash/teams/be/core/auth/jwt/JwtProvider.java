e2951ceb1c00d131014acf58de5bb2136382c64a
dltmdalssssss <dltmdalssssss0520@gmail.com>
2023-05-12 12:52:58 +0900

    Fix: cicd 관련 설정 변경

--- src/main/java/smash/teams/be/core/auth/jwt/JwtProvider.java
+++ src/main/java/smash/teams/be/core/auth/jwt/JwtProvider.java
@@ -5,6 +5,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import smash.teams.be.model.user.User;
 
@@ -16,8 +17,8 @@ public class JwtProvider {
     private static final int EXP = 1000 * 60 * 60* 24; // 24시간
     public static final String TOKEN_PREFIX = "Bearer "; // 스페이스 필요함
     public static final String HEADER = "Authorization";
-    private static final String SECRET = "메타코딩";
-    //private static final String SECRET = System.getenv("HS512_SECRET");
+    @Value("${JWT_SECRET_KEY}")
+    private static String SECRET;
 
     public static String create(User user) {
         String jwt = JWT.create()
